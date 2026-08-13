f1cd03f0acd5cfac349df0c2308af7bc4afe5a4e
Patrick Corcoran <patrickcorcoran10@gmail.com>
2026-08-13 14:14:04 -0500

    create-topic created

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,10 @@
 'use server'
 
 
-export async function createTopic() {
+export async function createTopic(formData:FormData) {
     // todo: revalidate the homepage
+    const name = formData.get('name')
+    const description = formData.get('description')
+
+    console.log(name, description)
 }
