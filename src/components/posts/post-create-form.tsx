c7095da0a5c3383258a576e99c02898abc28bc98
Patrick Corcoran <patrickcorcoran10@gmail.com>
2026-08-19 10:41:12 -0500

    building out the post functionality and validation

---
+++ src/components/posts/post-create-form.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import {useActionState, startTransition} from 'react'
+import {
+    Input,
+    Button,
+    Textarea,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    Form
+} from '@nextui-org/react'
+
+import * as actions from '@/actions'
+import FormButton from '@/components/common/form-button'
+
+export default function PostCreateForm() {
+     const [formState, action, isPending] = useActionState(actions.createPost, {
+        errors: {},
+      });
+
+       function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+          event.preventDefault();
+          const formData = new FormData(event.currentTarget);
+          startTransition(() => {
+            action(formData);
+          });
+        }
+    return (
+        <Popover placement='left'>
+            <PopoverTrigger>
+                <Button color='primary'>Create a Post</Button>
+            </PopoverTrigger>
+            <PopoverContent>
+                <Form onSubmit={handleSubmit}>
+                    <div className='flex flex-col gap-4 p-4 w-80'>
+                        <h3 className='text-lg'>Create a Post</h3>
+                        <Input 
+                        name="title" 
+                        label="Title" 
+                        labelPlacement="outside" 
+                        placeholder="Title" 
+                        isInvalid={!!formState.errors.title}
+                        errorMessage={formState.errors.title?.join(", ")}
+                        />
+                        <Textarea 
+                        name='content'  
+                        label="Content" 
+                        labelPlacement="outside" 
+                        placeholder="Content"
+                        isInvalid={!!formState.errors.content}
+                        errorMessage={formState.errors.content?.join(", ")}
+                        />
+
+                        {formState.errors._form ? <div className='rounded p-2 bg-red-200 border border-red-400'>{formState.errors._form.join(', ')}</div> : null}
+                        <FormButton isLoading={isPending}>Create Post</FormButton>
+                    </div>
+                </Form>
+            </PopoverContent>
+        </Popover>
+    )
+}
